e4f4394fec975b7249fe5a47c4d511d5a1282659
rcdmk <rcdmk@dtavares.com>
2016-04-18 20:24:12 -0300

    Created the autenticacao controller with it's service

---
+++ cs-node/controllers/autenticacao.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+
+var express = require('express');
+var mongoose = require('mongoose');
+
+var utils = require('../utils');
+var AutenticacaoService = require('../services/autenticacao');
+var UsuarioModel = require('../models/usuario');
+
+var router = express.Router();
+var service = new AutenticacaoService(UsuarioModel);
+
+
+// POST /autenticacao
+router.post('/', function (req, res) {
+    // validação de entrada
+    req.checkBody('email', 'Informe o email').notEmpty().isEmail().withMessage('E-mail inválido');
+    req.checkBody('senha', 'Informe a senha').notEmpty();
+    
+    var erros = req.validationErrors();
+
+    if (erros) {
+        utils.formatarErrosValidacao(res, erros);
+    } else {
+        var dados = {
+            email: req.body.email,
+            senha: req.body.senha
+        };
+        
+        service.autenticarUsuario(dados, function (err, data) {
+            if (err) {
+                if (err.name === 'UsuarioOusenhaInvalidos') {
+                    err.status = 401;
+                    req.next(err);
+                } else {
+                    utils.retornarErro503(err, req.next);
+                }                
+            } else {
+                res.json(data);
+            }
+        });
+    }
+});
+
+module.exports = router;
---
+++ cs-node/services/autenticacao.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+
+/**
+ * Serviço para autenticação de usuários
+ * @param model {Object} Uma instância de de um modelo para banco de dados
+ */
+var AutenticacaoService = function AutenticacaoService(model) {
+    this.db = model;
+};
+
+/**
+ * Autentica um usuário
+ * @param dados {Object} Um objeto com os dados do usuário para registrar
+ *  @option email {String} O e-mail do usuário
+ *  @option senha {String} A senha do usuário
+ */
+AutenticacaoService.prototype.autenticarUsuario = function AutenticarUsuario(dados, cb) {
+    if (!dados.email || !dados.senha) {
+        return cb(new Error('Usuário e/ou senha inválidos'));
+    }
+
+    this.db.findOne({ email: dados.email, senha: this.db.prototype.codificarSenha(dados.senha) }, function (err, usuario) {
+        if (err || !usuario) {
+            var erro = new Error('Usuário e/ou senha inválidos');
+            erro.name = 'UsuarioOusenhaInvalidos';
+
+            return cb(erro);
+        } else {
+            usuario.ultimo_login = Date.now();
+            
+            usuario.save(function (err, dados) {
+                if (err) {
+                    return cb(err);
+                }
+
+                cb(null, usuario.pegarJSON());
+            });
+        }
+    });
+};
+
+
+module.exports = AutenticacaoService;
