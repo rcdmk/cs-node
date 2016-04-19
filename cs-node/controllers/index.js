bf620e4892f1ef9ed83d024e5ba7b3ec6a928b0c
rcdmk <rcdmk@dtavares.com>
2016-04-19 01:28:51 -0300

    Implemented unit tests for the controllers module

--- cs-node/controllers/index.js
+++ cs-node/controllers/index.js
@@ -5,27 +5,34 @@ var debug = require('debug')('cs-node: ' + process.pid);
 var mongoose = require('mongoose');
 
 /**
+ * Carrega arquivos de controllers
+ */
+var carregaControllerDoArquivo = function carregaControllerDoArquivo(app, arquivo) {
+	if (arquivo !== 'index.js' && arquivo.substr(-3) === '.js') {
+		var nome = arquivo.substr(0, arquivo.length - 3);
+		var controller = require('./' + arquivo);
+		
+		app.use('/' + nome, controller(app, mongoose));
+
+		debug('Carregadas rotas de /' + nome);
+	}
+};
+
+
+/**
  * Carrega todos os controllers da pasta controllers
  * @param app {Object} Uma instância de uma aplicação Express
  * @param done {Function} Uma função de callback para executar ao concluir execução
  */
 var CarregarControllers = function CarregarControllers(app, done) {
-    fs.readdir(__dirname, function (err, arquivos) {
+    fs.readdir('./', function (err, arquivos) {
         if (err) {
             return done(err);
         }
 
-        var nome, controller;
-
-        arquivos.forEach(function carregaControllerDoArquivo(arquivo) {
-            if (arquivo !== 'index.js' && arquivo.substr(-3) === '.js') {
-                nome = arquivo.substr(0, arquivo.length - 3);
-                controller = require('./' + arquivo);
-
-                app.use('/' + nome, controller(app, mongoose));
-                debug('Carregadas rotas de /' + nome);
-            }
-        });
+		arquivos.forEach(function (arquivo) {
+			carregaControllerDoArquivo(app, arquivo);
+		});
 
         done();
     });
