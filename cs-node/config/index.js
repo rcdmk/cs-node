e3811dbb209915933571afd7463f3f5e325d347c
rcdmk <rcdmk@dtavares.com>
2016-04-19 00:16:20 -0300

    Included unit tests for the config module

--- cs-node/config/index.js
+++ cs-node/config/index.js
@@ -5,6 +5,11 @@ var configuracoes = {
     production: require('./PRD.json')
 };
 
+/**
+ * Retorna as configurações do ambiente informado
+ * @param ambiente {String} O nome do ambiente para obter a configuração
+ *	@remarks Caso não seja fornecido um ambiente ou seja fornecido um ambiente inválido, será assumido o ambiente de desenvolvimento (development)
+ */
 var Config = function Config(ambiente) {
     this.ambiente = ambiente || 'development';
     
